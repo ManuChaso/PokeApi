32d64c2929d5f186fea9366e0685eabcc28e7f92
KobayashiMauro <auroravalentinivalea@icloud.com>
2025-06-24 12:49:48 +0200

    aurora: notfound

--- src/utils/router.js
+++ src/utils/router.js
@@ -1,17 +1,17 @@
-import Home from "../Pages/Home/Home";
-import PokeList from "../Pages/PokeList/PokeList";
+import Home from '../Pages/Home/Home';
+import notfound from '../Pages/NotFound/notfound';
+import PokeList from '../Pages/PokeList/PokeList';
 
 const routes = {
-    '/': Home,
-    '/poke-list': PokeList
-}
+  '/': Home,
+  '/poke-list': PokeList,
+};
 
+export function router() {
+  const path = window.location.pathname;
+  const route = routes[path] || notfound;
+  const main = document.querySelector('#main-container');
+  main.innerHTML = '';
 
-export function router(){
-    const path = window.location.pathname;
-    const route = routes[path];
-    const main = document.querySelector('#main-container');
-    main.innerHTML = '';
-
-    main.appendChild(route())
+  main.appendChild(route());
 }
