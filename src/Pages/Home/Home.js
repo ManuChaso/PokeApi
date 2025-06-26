7473cb20001300a951a0bcee6e35983a9ee7d7c7
ManuChaso <manuc.chaso@gmail.com>
2025-06-26 12:51:01 +0200

    manu: pokedex try...

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,9 +1,48 @@
 import './Home.css';
 
+import pokedexImage from '../../../assets/images/pokedex.png';
+
 export default function Home(){
     const homeContainer = document.createElement('section');
     homeContainer.className = 'home-container';
 
 
+
+    const pokedex = document.createElement('img');
+    pokedex.src = pokedexImage;
+    pokedex.className = 'pokedex';
+
+    const hud = document.createElement('div');
+    hud.className = 'pokedex-hud';
+
+
+
+    hud.innerHTML = `
+        <div class="pokedex-screen-one">
+            <p class="pokemon-num">25</p>
+            <img class="pokemon-image" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/35.png" alt="">
+        </div>
+
+        <div class="pokedex-screen-two">
+            <p class="pokemon-name">Name</p>
+            <p class="pokemon-type">Type</p>
+        </div>
+
+        <div class="pokedex-buttons">
+            <button class="play-sound"></button>
+            <button class="prev-pokemon"></button>
+            <button class="next-pokemon"></button>
+        </div>
+    `
+
+
+    homeContainer.append(pokedex, hud);
+
+
+
+    window.addEventListener('resize', () => {
+        hud.style.width = `${pokedex.clientWidth}px`;
+        hud.style.height = `${pokedex.clientHeight}px`;
+    });
     return homeContainer
 }
